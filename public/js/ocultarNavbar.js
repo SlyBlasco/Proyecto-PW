1ced9cbf0132a462e1227d9843451a66e9fb1b8c
SlyBlasco <luxinthehouse1477@gmail.com>
2025-11-04 20:59:48 -0700

    Se agrego las fotos y videos para la galeria. Asi como su logica para rellenarlo.

--- public/js/ocultarNavbar.js
+++ public/js/ocultarNavbar.js
@@ -4,13 +4,16 @@ let lastScrollTop = 0;
 const navbar = document.getElementById("navbar");
 
 window.addEventListener("scroll", () => {
-    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-    if (currentScroll > lastScrollTop) {
-        navbar.classList.add("fade-out");
-        navbar.classList.remove("fade-in");
-    } else {
-        navbar.classList.add("fade-in");
-        navbar.classList.remove("fade-out");
-    }
+    // Use window.pageYOffset for most browsers; fallback to document.documentElement.scrollTop for older browsers
+        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (navbar) {
+        if (currentScroll > lastScrollTop) {
+            navbar.classList.add("fade-out");
+            navbar.classList.remove("fade-in");
+        } else {
+            navbar.classList.add("fade-in");
+            navbar.classList.remove("fade-out");
+        }
+    // Reset negative scroll values to zero to avoid unexpected behavior
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-}, false);
+}}, false);
